90ec69dddf4572e3e1923835a2581403dfad0f79
sanjeetaT <sanjeetadthapa@gmail.com>
2021-12-08 13:00:25 -0800

    video Project

---
+++ Video/Video.Website/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+document.getElementById("animate");
+let pos = 0;
+clearInterval(id);
+id = setInterval(frame, 5);
+function frame() {
+    if (pos == 350) {
+        clearInterval(id);
+    } else {
+        pos++;
+        elem.style.top = pos + "px";
+        elem.style.left = pos + "px";
+    }
+}
