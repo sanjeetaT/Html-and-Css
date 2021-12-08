b674fac5f6b58790872d8e64710f96a347385126
sanjeetaT <sanjeetadthapa@gmail.com>
2021-12-08 14:49:17 -0800

    make animation

--- Video/Video.Website/wwwroot/js/site.js
+++ Video/Video.Website/wwwroot/js/site.js
@@ -2,16 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-document.getElementById("animate");
-let pos = 0;
-clearInterval(id);
-id = setInterval(frame, 5);
-function frame() {
-    if (pos == 350) {
-        clearInterval(id);
-    } else {
-        pos++;
-        elem.style.top = pos + "px";
-        elem.style.left = pos + "px";
+var id = null;
+function myMove() {
+    var elem = document.getElementById("myAnimation");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
     }
 }
